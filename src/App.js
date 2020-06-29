6934f33577c1c5d9afa6951e4a0f3ba733e13f01
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-06-29 22:02:40 +0300

    Update design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Header() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className = "container" style = {{
+        backgroundColor: 'black',
+        width: '100%',
+        height: '10%',
+        background: 'rgba(0, 0, 0, 0.9)'
+      }}>
+      <div className = "col-lg-11">
+      <header className = "head-component">
+          <h3 className = "brand" style = {{
+              align: 'left',
+              color: '#ffffff',
+              fontFamily: 'Lobster'
+            }}>
+            Cigan Oliviu-David
+          </h3>
       </header>
+      </div>
+      <div className = "col-md-1">
+        <header class = 'head-component'>
+          <i id = "facebook" class = "fa fa-facebook-official" aria-hidden = "true"></i>
+          <i id = "instagram" class = "fa fa-instagram" aria-hidden = "true"></i>
+          <i id = "youtube" class = "fa fa-youtube-play" aria-hidden = "true"></i>
+        </header>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Header;

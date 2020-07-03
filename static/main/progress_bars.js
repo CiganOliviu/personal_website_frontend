d32ff3a46b34b66ff32d85932484439768ac7ccd
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-07-03 21:02:37 +0300

    Update frontend

--- static/main/progress_bars.js
+++ static/main/progress_bars.js
@@ -3,11 +3,13 @@ var forEach = function (array, callback, scope) {
 		callback.call(scope, i, array[i]);
 	}
 };
-window.onload = function(){
+window.onscroll = function(){
+
 	var max = -219.99078369140625;
+
 	forEach(document.querySelectorAll('.progress'), function (index, value) {
 	percent = value.getAttribute('data-progress');
-		value.querySelector('.fill').setAttribute('style', 'stroke-dashoffset: ' + ((100 - percent) / 100) * max);
-		value.querySelector('.value').innerHTML = percent + '%';
+    value.querySelector('.fill').setAttribute('style', 'stroke-dashoffset: ' + ((100 - percent) / 100) * max);
+    value.querySelector('.value').innerHTML = percent + '%';
 	});
 }
